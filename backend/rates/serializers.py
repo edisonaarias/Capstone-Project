4ecdebe9bb1acf923933967c082bd0248f58696d
unknown <edisonalexanderarias@gmail.com>
2022-08-05 21:22:04 -0700

    added another model

--- backend/rates/serializers.py
+++ backend/rates/serializers.py
@@ -7,6 +7,6 @@ from .models import Rate
 class RateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rate
-        fields = ['id','Rates','professor_id']
+        fields = ['id','Rates','professor_id',"name"]
         depth = 1
         professor_id = serializers.IntegerField(write_only=True)
